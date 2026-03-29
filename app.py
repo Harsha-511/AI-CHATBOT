b001618acb78fb43309b76b86bd3dee23b2a833a
Harsha <harshavardhan.thulluri@gmail.com>
2026-03-30 00:19:01 +0530

    add runtime for render

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, jsonify
 from google import genai
 from datetime import datetime
 import time
+import os
 
 app = Flask(__name__)
 
@@ -64,4 +65,5 @@ def chat():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
